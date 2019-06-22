a61cc7b65c845cdad5eb80166d9a488c54f1cf11
Nic McPhee <mcphee@morris.umn.edu>
2019-06-21 22:39:06 -0500

    Start a `util` namespace
    I wanted my grammar work to be in its own
    namespace, and it turned out that I had
    dependencies on things in `core`. This
    required moving some of the "utility"
    functions into a new `util` namespace so
    they can be required by both `core` and the
    grammar work.
    
    I'm not 100% sure these are really `util`
    things – it might make more sense to have
    a `push-state` namespace and put them
    there.

--- src/propel/core.clj
+++ src/propel/core.clj
@@ -1,4 +1,5 @@
 (ns propel.core
+  (:require [propel.util :as u])
   (:gen-class))
 
 (def example-push-state
@@ -71,82 +72,29 @@
     (apply list lst)
     lst))
 
-(defn push-to-stack
-  "Pushes item onto stack in state"
-  [state stack item]
-  (update state stack conj item))
-
-(defn pop-stack
-  "Removes top item of stack."
-  [state stack]
-  (update state stack rest))
-
-(defn peek-stack
-  "Returns top item on a stack."
-  [state stack]
-  (if (empty? (get state stack))
-    :no-stack-item
-    (first (get state stack))))
-
-(defn empty-stack?
-  "Returns true if the stack is empty."
-  [state stack]
-  (empty? (get state stack)))
-
-(defn get-args-from-stacks
-  "Takes a state and a list of stacks to take args from. If there are enough args
-  on each of the desired stacks, returns a map of the form {:state :args}, where
-  :state is the new state and :args is a list of args from the stacks. If there
-  aren't enough args on the stacks, returns :not-enough-args."
-  [state stacks]
-  (loop [state state
-         stacks (reverse stacks)
-         args '()]
-    (if (empty? stacks)
-      {:state state :args args}
-      (let [stack (first stacks)]
-        (if (empty-stack? state stack)
-          :not-enough-args
-          (recur (pop-stack state stack)
-                 (rest stacks)
-                 (conj args (peek-stack state stack))))))))
-
-(defn make-push-instruction
-  "A utility function for making Push instructions. Takes a state, the function
-  to apply to the args, the stacks to take the args from, and the stack to return
-  the result to. Applies the function to the args (taken from the stacks) and pushes
-  the return value onto return-stack."
-  [state function arg-stacks return-stack]
-  (let [args-pop-result (get-args-from-stacks state arg-stacks)]
-    (if (= args-pop-result :not-enough-args)
-      state
-      (let [result (apply function (:args args-pop-result))
-            new-state (:state args-pop-result)]
-        (push-to-stack new-state return-stack result)))))
-
 ;;;;;;;;;
 ;; Instructions
 
 (defn in1
   "Pushes the input labeled :in1 on the inputs map onto the :exec stack."
   [state]
-  (push-to-stack state :exec (:in1 (:input state))))
+  (u/push-to-stack state :exec (:in1 (:input state))))
 
 (defn integer_+
   [state]
-  (make-push-instruction state +' [:integer :integer] :integer))
+  (u/make-push-instruction state +' [:integer :integer] :integer))
 
 (defn integer_-
   [state]
-  (make-push-instruction state -' [:integer :integer] :integer))
+  (u/make-push-instruction state -' [:integer :integer] :integer))
 
 (defn integer_*
   [state]
-  (make-push-instruction state *' [:integer :integer] :integer))
+  (u/make-push-instruction state *' [:integer :integer] :integer))
 
 (defn integer_%
   [state]
-  (make-push-instruction state
+  (u/make-push-instruction state
                          (fn [int1 int2]
                            (if (zero? int2)
                              int1
@@ -156,76 +104,76 @@
 
 (defn integer_=
   [state]
-  (make-push-instruction state = [:integer :integer] :boolean))
+  (u/make-push-instruction state = [:integer :integer] :boolean))
 
 (defn exec_dup
   [state]
-  (if (empty-stack? state :exec)
+  (if (u/empty-stack? state :exec)
     state
-    (push-to-stack state :exec (first (:exec state)))))
+    (u/push-to-stack state :exec (first (:exec state)))))
 
 (defn exec_if
   [state]
-  (make-push-instruction state
+  (u/make-push-instruction state
                          #(if %1 %3 %2)
                          [:boolean :exec :exec]
                          :exec))
 
 (defn boolean_and
   [state]
-  (make-push-instruction state #(and %1 %2) [:boolean :boolean] :boolean))
+  (u/make-push-instruction state #(and %1 %2) [:boolean :boolean] :boolean))
 
 (defn boolean_or
   [state]
-  (make-push-instruction state #(or %1 %2) [:boolean :boolean] :boolean))
+  (u/make-push-instruction state #(or %1 %2) [:boolean :boolean] :boolean))
 
 (defn boolean_not
   [state]
-  (make-push-instruction state not [:boolean] :boolean))
+  (u/make-push-instruction state not [:boolean] :boolean))
 
 (defn boolean_=
   [state]
-  (make-push-instruction state = [:boolean :boolean] :boolean))
+  (u/make-push-instruction state = [:boolean :boolean] :boolean))
 
 (defn string_=
   [state]
-  (make-push-instruction state = [:string :string] :boolean))
+  (u/make-push-instruction state = [:string :string] :boolean))
 
 (defn string_take
   [state]
-  (make-push-instruction state
+  (u/make-push-instruction state
                          #(apply str (take %1 %2))
                          [:integer :string]
                          :string))
 
 (defn string_drop
   [state]
-  (make-push-instruction state
+  (u/make-push-instruction state
                          #(apply str (drop %1 %2))
                          [:integer :string]
                          :string))
 
 (defn string_reverse
   [state]
-  (make-push-instruction state
+  (u/make-push-instruction state
                          #(apply str (reverse %))
                          [:string]
                          :string))
 
 (defn string_concat
   [state]
-  (make-push-instruction state
+  (u/make-push-instruction state
                          #(apply str (concat %1 %2))
                          [:string :string]
                          :string))
 
 (defn string_length
   [state]
-  (make-push-instruction state count [:string] :integer))
+  (u/make-push-instruction state count [:string] :integer))
 
 (defn string_includes?
   [state]
-  (make-push-instruction state clojure.string/includes? [:string :string] :boolean))
+  (u/make-push-instruction state clojure.string/includes? [:string :string] :boolean))
 
 ;;;;;;;;;
 ;; Interpreter
@@ -233,7 +181,7 @@
 (defn interpret-one-step
   "Takes a Push state and executes the next instruction on the exec stack."
   [state]
-  (let [popped-state (pop-stack state :exec)
+  (let [popped-state (u/pop-stack state :exec)
         first-raw (first (:exec state))
         first-instruction (if (symbol? first-raw)
                             (eval first-raw)
@@ -243,16 +191,16 @@
       (first-instruction popped-state)
       ;
       (integer? first-instruction)
-      (push-to-stack popped-state :integer first-instruction)
+      (u/push-to-stack popped-state :integer first-instruction)
       ;
       (string? first-instruction)
-      (push-to-stack popped-state :string first-instruction)
+      (u/push-to-stack popped-state :string first-instruction)
       ;
       (seq? first-instruction)
       (update popped-state :exec #(concat %2 %1) first-instruction)
       ;
       (or (= first-instruction true) (= first-instruction false))
-      (push-to-stack popped-state :boolean first-instruction)
+      (u/push-to-stack popped-state :boolean first-instruction)
       ;
       :else
       (throw (Exception. (str "Unrecognized Push instruction in program: "
@@ -409,7 +357,7 @@
         (zero? (:total-error (first evaluated-pop))) (println "SUCCESS")
         (>= generation max-generations) nil
         :else (recur (inc generation)
-                     (repeatedly population-size 
+                     (repeatedly population-size
                                  #(new-individual evaluated-pop argmap)))))))
 
 ;;;;;;;;;
@@ -436,7 +384,7 @@
         inputs (range -10 11)
         correct-outputs (map target-function inputs)
         outputs (map (fn [input]
-                       (peek-stack
+                       (u/peek-stack
                         (interpret-program
                          program
                          (assoc empty-push-state :input {:in1 input})
@@ -464,7 +412,7 @@
         inputs ["GCG" "GACAG" "AGAAG" "CCCA" "GATTACA" "TAGG" "GACT"]
         correct-outputs [false false false false true true true]
         outputs (map (fn [input]
-                       (peek-stack
+                       (u/peek-stack
                         (interpret-program
                          program
                          (assoc empty-push-state :input {:in1 input})
@@ -500,4 +448,3 @@
                                         (map read-string args)))
                           [:error-function]
                           #(if (fn? %) % (eval %))))))
-
---
+++ src/propel/util.clj
@@ -0,0 +1,54 @@
+(ns propel.util)
+
+(defn empty-stack?
+  "Returns true if the stack is empty."
+  [state stack]
+  (empty? (get state stack)))
+
+(defn pop-stack
+  "Removes top item of stack."
+  [state stack]
+  (update state stack rest))
+
+(defn peek-stack
+  "Returns top item on a stack."
+  [state stack]
+  (if (empty? (get state stack))
+    :no-stack-item
+    (first (get state stack))))
+
+(defn push-to-stack
+  "Pushes item onto stack in state"
+  [state stack item]
+  (update state stack conj item))
+
+(defn get-args-from-stacks
+  "Takes a state and a list of stacks to take args from. If there are enough args
+  on each of the desired stacks, returns a map of the form {:state :args}, where
+  :state is the new state and :args is a list of args from the stacks. If there
+  aren't enough args on the stacks, returns :not-enough-args."
+  [state stacks]
+  (loop [state state
+         stacks (reverse stacks)
+         args '()]
+    (if (empty? stacks)
+      {:state state :args args}
+      (let [stack (first stacks)]
+        (if (empty-stack? state stack)
+          :not-enough-args
+          (recur (pop-stack state stack)
+                 (rest stacks)
+                 (conj args (peek-stack state stack))))))))
+
+(defn make-push-instruction
+  "A utility function for making Push instructions. Takes a state, the function
+  to apply to the args, the stacks to take the args from, and the stack to return
+  the result to. Applies the function to the args (taken from the stacks) and pushes
+  the return value onto return-stack."
+  [state function arg-stacks return-stack]
+  (let [args-pop-result (get-args-from-stacks state arg-stacks)]
+    (if (= args-pop-result :not-enough-args)
+      state
+      (let [result (apply function (:args args-pop-result))
+            new-state (:state args-pop-result)]
+        (push-to-stack new-state return-stack result)))))
