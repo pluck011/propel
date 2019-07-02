06397fc15d18561edd81482f7d883f4033ee7c89
Bill Tozier <bill@vagueinnovation.com>
2019-07-02 16:21:07 -0400

    some work on readme

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject propel "0.3.0"
+(defproject propel "0.4.0"
   :description "Bill Tozier's CLJSification of Lee Spector's Plusification of Tom Helmuth's small Push implementation in Clojure."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :plugins [[lein-gorilla "0.4.0"]]
   :main ^:skip-aot propel.core
   :target-path "target/%s"
